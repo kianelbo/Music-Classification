72ddc0e37e40e85d581c9b5b20ab6f391abd4a03
Kian Eliasi <kian.elbo@gmail.com>
2019-07-18 16:53:29 +0430

    added predict method for features based model

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from features.extractor import make_training_data
+from features.extractor import make_training_data, extract
 from model.features_based import FeaturesBasedModel
 from model.spectrogram_based import SpectrogramBasedModel
 
 # make_training_data('D:/dataset3/')
 
-# FeaturesBasedModel().train('save/data.csv')
-SpectrogramBasedModel().train('save/X.pickle', 'save/y.pickle')
+model = FeaturesBasedModel()
+model.train('save/data.csv')
+features_set, _ = extract('D:/Blues Leave Me Alone.mp3')
+model.predict(features_set)
+# SpectrogramBasedModel().train('save/X.pickle', 'save/y.pickle')
--- model/features_based.py
+++ model/features_based.py
@@ -1,3 +1,4 @@
+import pickle
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -9,8 +10,10 @@ class FeaturesBasedModel:
     def __init__(self, model_path=None):
         if model_path is not None:
             self.model = keras.models.load_model(model_path)
+            self.encoder = pickle.load(open('save/enc_classes.pickle', 'rb'))
         else:
             self.model = None
+            self.encoder = LabelEncoder()
 
     def train(self, csv_file):
         data = pd.read_csv(csv_file, encoding='ISO-8859-1')
@@ -19,8 +22,12 @@ class FeaturesBasedModel:
 
         data.drop(['title'], axis=1, inplace=True)
         genre_list = data.iloc[:, -1]
-        encoder = LabelEncoder()
-        y = encoder.fit_transform(genre_list)
+
+        y = self.encoder.fit_transform(genre_list)
+        pickle_out = open('save/enc_classes.pickle', 'wb')
+        pickle.dump(self.encoder, pickle_out)
+        pickle_out.close()
+        n_classes = len(self.encoder.classes_)
 
         X = StandardScaler().fit_transform(np.array(data.iloc[:, :-1], dtype=float))
 
@@ -30,10 +37,10 @@ class FeaturesBasedModel:
         self.model.add(keras.layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
         self.model.add(keras.layers.Dense(128, activation='relu'))
         self.model.add(keras.layers.Dense(64, activation='relu'))
-        self.model.add(keras.layers.Dense(10, activation='softmax'))
+        self.model.add(keras.layers.Dense(n_classes, activation='softmax'))
         self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-        history = self.model.fit(X_train, y_train, epochs=30)
+        history = self.model.fit(X_train, y_train, epochs=20)
         print('training done')
 
         test_loss, test_acc = self.model.evaluate(X_test, y_test)
@@ -41,12 +48,15 @@ class FeaturesBasedModel:
 
         self.model.save('save/fb.model')
 
-    def predict(self):
-        pass
+    def predict(self, features_set):
+        samples = []
+        for fs in features_set:
+            samples.append(fs.split(' '))
 
-# sample = [create_features('../../x.wav').split(' ')]
-# scaled_sample = scaler.fit_transform(np.array(sample, dtype=float))
-# prediction = np.argmax(model.predict(scaled_sample))
-# print(prediction)
-#
-# print(encoder.inverse_transform([prediction]))
+        scaled_samples = StandardScaler().fit_transform(np.array(samples, dtype=float))
+        prediction = self.model.predict(scaled_samples)
+        prediction = np.argmax(prediction, axis=1)
+        # prediction = prediction.sum(axis=0)
+        print(prediction)
+        # prediction = np.argmax(prediction)
+        print(self.encoder.inverse_transform(prediction))
